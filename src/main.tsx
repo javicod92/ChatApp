d6d619f576992d7d5d592423745c049ca5e7f582
javicod92 <javi.javier.jc@gmail.com>
2025-05-27 01:00:37 -0300

    feat: add global CSS and empty layout foundation.
    - Add empty layout component as foundation.
    - Prepare base structure for future development.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { Layout } from "./layouts";
+import "./styles/global.css";
+
+// import "./index.css";
+// import App from "./App.tsx";
 // import IconGallery from "./IconGallery.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <IconGallery /> */}
+    <Layout />
   </StrictMode>
 );
