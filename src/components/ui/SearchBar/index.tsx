dc33cb734c79800a2405c7814f6232b7c5f817d4
javicod92 <javi.javier.jc@gmail.com>
2025-05-30 18:17:37 -0300

    Adjust styling for SearchBar component

--- src/components/ui/SearchBar/index.tsx
+++ src/components/ui/SearchBar/index.tsx
@@ -1,3 +1,27 @@
+import { SearchRefreshedThin } from "../Icons";
+import styles from "./SearchBar.module.css";
+
 export default function SearchBar() {
-  return <div></div>;
+  return (
+    <div className={styles.SearchContainer}>
+      <div className={styles.searchWrapper}>
+        <button className={styles.searchButton}>
+          <span aria-hidden="true">
+            <SearchRefreshedThin />
+          </span>
+        </button>
+        <div className={styles.SearchInput}>
+          <label htmlFor="search-input" style={{ display: "none" }}>
+            Search Chat
+          </label>
+          <input
+            id="search-input"
+            type="text"
+            className={styles.InputField}
+            placeholder="Buscar un chat o iniciar uno nuevo"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
