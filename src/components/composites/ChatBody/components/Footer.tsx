4a3a0ad1267499d7660e9663e9f1cfe89f6c33e3
javicod92 <javi.javier.jc@gmail.com>
2025-06-17 18:06:43 -0300

    feat: implements a send button to display the user's message filled in the text area

--- src/components/composites/ChatBody/components/Footer.tsx
+++ src/components/composites/ChatBody/components/Footer.tsx
@@ -1,12 +1,23 @@
+import { useRef } from "react";
 import { RoundedButton } from "../../../ui/Buttons";
 import { Expressions, PlusRounded, SendFilled } from "../../../ui/Icons";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
+  const inputRef = useRef<HTMLDivElement>(null);
+
   const handleInput = (event: React.FormEvent<HTMLDivElement>) => {
     const el = event.currentTarget;
     if (el.innerHTML === "<br>" || el.innerHTML === "&nbsp;") {
       el.innerHTML = "";
+    }
+  };
+
+  const handleSend = () => {
+    const message = inputRef.current?.innerText.trim();
+    if (message) {
+      alert(message);
+      inputRef.current!.innerText = "";
     }
   };
 
@@ -27,9 +38,10 @@ export default function Footer() {
         role="textbox"
         aria-placeholder="Escribe un mensaje"
         onInput={handleInput}
+        ref={inputRef}
       ></div>
 
-      <RoundedButton aria-label="Send message">
+      <RoundedButton aria-label="Send message" handleClick={handleSend}>
         <SendFilled />
       </RoundedButton>
     </div>
