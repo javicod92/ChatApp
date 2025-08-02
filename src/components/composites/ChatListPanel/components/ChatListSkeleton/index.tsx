8a80552729c2a58a8bdfd5f5e922baa3595b4729
javicod92 <javi.javier.jc@gmail.com>
2025-08-01 23:58:28 -0300

    improve ChatList Skeleton component and adds project screenshots

--- src/components/composites/ChatListPanel/components/ChatListSkeleton/index.tsx
+++ src/components/composites/ChatListPanel/components/ChatListSkeleton/index.tsx
@@ -2,11 +2,19 @@ import styles from "./ChatListSkeleton.module.css";
 
 export default function Skeleton() {
   return (
-    <div className={styles.skeletonCard}>
-      <div className={styles.skeletonAvatar} />
-      <div className={styles.skeletonContent}>
-        <div className={styles.skeletonLineShort} />
-        <div className={styles.skeletonLineLong} />
+    <div className={`${styles.selected} ${styles.chatCard}`}>
+      <div className={styles.avatarContainer}>
+        <div className={styles.avatar}></div>
+      </div>
+      <div className={styles.cardDescription}>
+        <div className={styles.descriptionContact}>
+          <div className={styles.contact}>
+            <div></div>
+          </div>
+        </div>
+        <div className={styles.lastMessage}>
+          <div></div>
+        </div>
       </div>
     </div>
   );
